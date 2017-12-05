b3be36f294cc4b86de5663c1d5e0231b2bf19ff2
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2017-12-04 18:22:48 -0600

    End of Class Update

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class InventoryMenu : MonoBehaviour {
+    [SerializeField]
+    private GameObject inventoryMenuPanel;
+
+    [SerializeField]
+    private FirstPersonController firstPersonController;
+
+    private bool isMenuShowing
+    {
+        get { return inventoryMenuPanel.activeSelf; }
+    }
+
+    public List<InventoryObject> PlayerInventory { get; private set; }
+
+    private void Awake()
+    {
+        PlayerInventory = new List<InventoryObject>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        HideMenu();
+	}
+    private void Update()
+    {
+        HandleInput();
+        UpdateCursor();
+    }
+
+    private void HideMenu()
+    {
+        inventoryMenuPanel.SetActive(false);
+        firstPersonController.enabled = true;
+    }
+    private void ShowMenu()
+    {
+        inventoryMenuPanel.SetActive(true);
+        firstPersonController.enabled = false;
+    }
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isMenuShowing)
+                HideMenu();
+            else
+                ShowMenu();
+        }
+    }
+    private void UpdateCursor()
+    {
+        if (isMenuShowing)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour, IActivatable {
+    [SerializeField]
+    private string nameText;
+    [SerializeField]
+    private string descriptionText;
+
+    private MeshRenderer meshRenderer;
+    private Collider collider;
+    private List<InventoryObject> playerInventory;
+
+    public string NameText
+    {
+        get
+        {
+            return nameText;
+        }
+    }
+
+    public void DoActivate()
+    {
+        //add to inventory Menu
+        playerInventory.Add(this);
+        //remove from 3D world
+        meshRenderer.enabled = false;
+        collider.enabled = false;
+        throw new System.NotImplementedException();
+    }
+    
+    // Use this for initialization
+    void Start ()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
+        playerInventory = FindObjectOfType<InventoryMenu>().PlayerInventory;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
