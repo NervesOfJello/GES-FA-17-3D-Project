69eaa2a080ddb791555ac25e592c2445f9ac80b1
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2017-12-11 10:05:45 -0600

    Functional Build Commit

---
+++ Assets/Scripts/Button Scripts/MenuButtonBehaviors.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonBehaviors : MonoBehaviour {
+    private AudioSource audio;
+
+    public void NavigateToMenu()
+    {
+        audio.Play();
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void NavigateToCredits()
+    {
+        audio.Play();
+        SceneManager.LoadScene("CreditsScene");
+    }
+    public void StartGame()
+    {
+        audio.Play();
+        SceneManager.LoadScene("Level1");
+    }
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+}
